f2d7a4f126de71e4e667aef64772c9ad2febbba3
wkflanders <wflan@seas.upenn.edu>
2024-11-21 23:58:50 -0500

    icon exports

---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import home from "../assets/icons/home.png";
+import skills from "../assets/icons/skills.png";
+import friends from "../assets/icons/friends.png";
+import rewards from "../assets/icons/rewards.png";
+
+export default {
+    home,
+    skills,
+    friends,
+    rewards,
+};

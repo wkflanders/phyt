246acb90bda35cb4c0b06b20b32c63b237d15522
wkflanders <wflan@seas.upenn.edu>
2024-11-21 23:58:09 -0500

    tailwind config styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        phyt_blue: "#00F6FB",
+        phyt_red: "#FE205D",
+        phyt_bg: "#101010"
+      },
+      fontFamily: {
+        incextralight: ["Inconsolata-ExtraLight"],
+        inclight: ["Inconsolata-Light"],
+        incregular: ["Inconsolata-Regular"],
+        incmedium: ["Inconsolata-Medium"],
+        incsemibold: ["Inconsolata-SemiBold"],
+        incbold: ["Inconsolata-Bold"],
+        incextrabold: ['Inconsolata-ExtraBold'],
+        incblack: ['Inconsolata-Black'],
+      }
+    },
   },
   plugins: [],
 }

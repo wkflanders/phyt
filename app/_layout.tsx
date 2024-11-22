0c71fa73aeeff5e69155d81ea421dc186ceadbf2
wkflanders <wflan@seas.upenn.edu>
2024-11-21 19:41:20 -0500

    load fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,31 @@
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
-import React from 'react';
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+
 import "../global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 const RootLayout = () => {
+    const [fontsLoaded, error] = useFonts({
+        "Inconsolata-ExtraLight": require("../assets/fonts/Inconsolata-ExtraLight.ttf"),
+        "Inconsolata-Light": require("../assets/fonts/Inconsolata-Light.ttf"),
+        "Inconsolata-Regular": require("../assets/fonts/Inconsolata-Regular.ttf"),
+        "Inconsolata-Medium": require("../assets/fonts/Inconsolata-Medium.ttf"),
+        "Inconsolata-Bold": require("../assets/fonts/Inconsolata-Bold.ttf"),
+        "Inconsolata-Semibold": require("../assets/fonts/Inconsolata-SemiBold.ttf"),
+        "Inconsolata-ExtraBold": require("../assets/fonts/Inconsolata-ExtraBold.ttf"),
+        "Inconsolata-Black": require("../assets/fonts/Inconsolata-Black.ttf"),
+    });
+
+    useEffect(() => {
+        if (error) throw error;
+        if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded && !error) return null;
+
     return (
         <Stack>
             <Stack.Screen name="index" />

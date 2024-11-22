33ed0931e89134d26a8e3d6e9ce5983d129e19ed
wkflanders <wflan@seas.upenn.edu>
2024-11-21 19:11:55 -0500

    nativewind setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const index = () => {
+    return (
+        <View>
+            <Text className="bg-red-500">index</Text>
+        </View>
+    );
+};
+
+export default index;

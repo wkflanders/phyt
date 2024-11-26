8ae7a3f4c90aa9b593e67ab31f1d2df56103eb8a
wkflanders <wflan@seas.upenn.edu>
2024-11-25 19:30:54 -0500

    sign in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,72 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { useState } from 'react';
+
+import { View, Text, ScrollView, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
+
+import { images } from "@/constants";
+
+import FormField from '@/components/FormField';
+import Button from '@/components/Button';
 
 const SignIn = () => {
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const submit = () => {
+
+    };
+
     return (
-        <View>
-            <Text>SignIn</Text>
-        </View>
+        <SafeAreaView className="bg-phyt_blue h-full">
+            <ScrollView>
+                <View className="justify-center min-h-[80vh]">
+                    <View className="w-full justify-center items-center px-4">
+                        <Text className="color-phyt_red font-incsemibold text-2xl pb-2">
+                            welcome to
+                        </Text>
+                        <Image
+                            source={images.onboard_logo}
+                            className='w-[200px] h-[84px]'
+                            resizeMode="contain"
+                        />
+                    </View>
+                    <View className="w-full justify-center items-center px-12">
+                        <FormField
+                            title="Email"
+                            value={form.email}
+                            handleChangeText={(e) => setForm({ ...form, email: e })}
+                            otherStyles="mt-5"
+                            placeholder="EMAIL"
+                            keyboardType='email-address'
+                        />
+                        <FormField
+                            title="Password"
+                            value={form.password}
+                            handleChangeText={(e) => setForm({ ...form, password: e })}
+                            placeholder="PASSWORD"
+                            otherStyles="mt-5"
+                        />
+
+                        <Button
+                            title="LOGIN"
+                            handlePress={submit}
+                            containerStyles="mt-10"
+                            isLoading={isSubmitting}
+                        />
+
+                        <View className="justify-center pt-5 flex-row gap-2">
+                            <Link href="/sign-up" className="font-incsemibold color-phyt_red underline underline-offset-5">
+                                or sign up
+                            </Link>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 

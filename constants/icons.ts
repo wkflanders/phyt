0e6d4565ed3a39d197e266102ec014c3fc36587e
wkflanders <wflan@seas.upenn.edu>
2024-11-25 19:31:28 -0500

    update constants

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,8 +1,10 @@
-import home from "../assets/icons/home.png";
-import skills from "../assets/icons/skills.png";
-import friends from "../assets/icons/friends.png";
-import challenges from "../assets/icons/challenges.png";
-import rewards from "../assets/icons/rewards.png";
+import home from "@/assets/icons/home.png";
+import skills from "@/assets/icons/skills.png";
+import friends from "@/assets/icons/friends.png";
+import challenges from "@/assets/icons/challenges.png";
+import rewards from "@/assets/icons/rewards.png";
+import eye from "@/assets/icons/eye.png";
+import eyehide from "@/assets/icons/eye-hide.png";
 
 export default {
     home,
@@ -10,4 +12,6 @@ export default {
     friends,
     challenges,
     rewards,
+    eye,
+    eyehide,
 };

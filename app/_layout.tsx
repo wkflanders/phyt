e90b47c7ae1a5553a5c0cd28b5a40cbfa705dd04
wkflanders <wflan@seas.upenn.edu>
2024-12-02 03:37:38 -0500

    add inter font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,15 @@ const RootLayout = () => {
         "Inconsolata-SemiBold": require("../assets/fonts/Inconsolata-SemiBold.ttf"),
         "Inconsolata-ExtraBold": require("../assets/fonts/Inconsolata-ExtraBold.ttf"),
         "Inconsolata-Black": require("../assets/fonts/Inconsolata-Black.ttf"),
+        "Inter-ExtraLight": require("../assets/fonts/Inter-ExtraLight.ttf"),
+        "Inter-Light": require("../assets/fonts/Inter-Light.ttf"),
+        "Inter-Thin": require("../assets/fonts/Inter-Thin.ttf"),
+        "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
+        "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+        "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+        "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+        "Inter-ExtraBold": require("../assets/fonts/Inter-ExtraBold.ttf"),
+        "Inter-Black": require("../assets/fonts/Inter-Black.ttf"),
     });
 
     useEffect(() => {

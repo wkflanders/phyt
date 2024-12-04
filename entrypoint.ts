cb711a62287457e559fd76e3267f1bc0068bd603
wkflanders <wflan@seas.upenn.edu>
2024-12-04 13:50:53 -0500

    thirdweb init

--- entrypoint.ts
+++ entrypoint.ts
@@ -2,5 +2,6 @@
 import 'fast-text-encoding';
 import 'react-native-get-random-values';
 import '@ethersproject/shims';
+import "@thirdweb-dev/react-native-adapter";
 // Then import the expo router
 import 'expo-router/entry';
--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,11 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_conditionNames = [
+    "react-native",
+    "browser",
+    "require",
+];
+
 module.exports = withNativeWind(config, { input: "./global.css" });

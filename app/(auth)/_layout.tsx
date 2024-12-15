df87b4d019eb8529b11f440187b1211f3d7c60e8
wkflanders <wflan@seas.upenn.edu>
2024-12-15 08:14:26 -0500

    add onboard screen to auth stack

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -12,6 +12,12 @@ const AuthLayout = () => {
                         headerShown: false
                     }}
                 />
+                <Stack.Screen
+                    name="onboard"
+                    options={{
+                        headerShown: false
+                    }}
+                />
             </Stack>
 
             <StatusBar backgroundColor="#161622" style="light" />

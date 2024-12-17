ddb1bb58a081f3e716e6a897e974d982c8fc70d8
wkflanders <wflan@seas.upenn.edu>
2024-12-17 01:10:45 -0500

    feed component into home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,14 +2,13 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '@/components/Header';
+import { Feed } from '@/features/social/components/Feed';
 
 const Home = () => {
     return (
         <SafeAreaView className="h-full w-full bg-black">
             <Header />
-            <Text className="color-white">
-                Hello
-            </Text>
+            <Feed />
         </SafeAreaView>
     );
 };

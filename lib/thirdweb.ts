6b3d94756ada53547df18fba4f6b9c53f6fd88f6
wkflanders <wflan@seas.upenn.edu>
2024-12-23 11:51:48 -0500

    thirdweb client

--- lib/thirdweb.ts
+++ lib/thirdweb.ts
@@ -1,21 +1,5 @@
-// import { ThirdwebSDK } from "@thirdweb-dev/react-native-adapter";
-// import { ethers } from "ethers";
-// import type { EIP1193Provider } from "@privy-io/expo";
+import { createThirdwebClient } from "thirdweb";
 
-// let sdk: ThirdwebSDK | null = null;
-
-// export async function initializeThirdwebSDK(provider: EIP1193Provider) {
-//     // Cast provider to 'any' to pass into Web3Provider (common practice)
-//     const ethersProvider = new ethers.providers.Web3Provider(provider as any);
-//     const signer = ethersProvider.getSigner();
-
-//     // Initialize the Thirdweb SDK with the signer
-//     sdk = new ThirdwebSDK(signer);
-// }
-
-// export function getThirdwebSDK(): ThirdwebSDK {
-//     if (!sdk) {
-//         throw new Error("Thirdweb SDK not initialized. Call initializeThirdwebSDK() after the wallet is connected.");
-//     }
-//     return sdk;
-// }
+export const client = createThirdwebClient({
+    clientId: "5a78c3fe1f7992a009d293cb1f951468",
+});

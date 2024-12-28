4b49a5cf3717f0f096d47452ab0d7bf1a3e0742c
wkflanders <wflan@seas.upenn.edu>
2024-12-28 10:46:23 -0500

    mapbox client

---
+++ lib/mapbox.ts
@@ -0,0 +1,5 @@
+import MapboxGL from '@rnmapbox/maps';
+
+MapboxGL.setAccessToken('pk.eyJ1Ijoid2tmbGFuZGVycyIsImEiOiJjbTU3OHlnazMzbng1Mm5vbzk1dDdtZmFsIn0.28_-OvlGRS4ew303vUa3Vw');
+
+export default MapboxGL;

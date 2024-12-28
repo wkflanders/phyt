cd3496b2e0c1661208c42579f7dfabce9df7a511
wkflanders <wflan@seas.upenn.edu>
2024-12-28 10:46:06 -0500

    mapbox entry point

--- entrypoint.ts
+++ entrypoint.ts
@@ -3,5 +3,6 @@ import 'fast-text-encoding';
 import 'react-native-get-random-values';
 import '@ethersproject/shims';
 import "@thirdweb-dev/react-native-adapter";
+import '@/lib/mapbox';
 // Then import the expo router
 import 'expo-router/entry';
